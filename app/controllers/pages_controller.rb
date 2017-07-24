10e728b3a53fd267848301bf2a2df9efa5cb1bd7
Mykl Clason <myklclason@gmail.com>
2017-07-24 07:18:36 +0000

    Generate pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def what-you-get
+  end
+
+  def leadership-jobs
+  end
+
+  def events
+  end
+
+  def free-ebook
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+
+  def newsletter-signup
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/what-you-get'
+
+  get 'pages/leadership-jobs'
+
+  get 'pages/events'
+
+  get 'pages/free-ebook'
+
+  get 'pages/blog'
+
+  get 'pages/contact'
+
+  get 'pages/newsletter-signup'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
