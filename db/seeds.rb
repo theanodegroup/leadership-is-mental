cbf250c0ae22bc956578b67737f5b72e835d15ef
Mykl Clason <myklclason@gmail.com>
2017-08-13 05:05:09 +0000

    Get UI working for carousel images and links

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,11 @@
 if Rails.env.development?
   5.times do |index|
     CarouselImage.create({
-      carousel: CarouselImage::CAROUSELS_INVERSE[CarouselImage::CAROUSEL_HOME_TOP],
+      carousel: CarouselImage::CAROUSEL_HOME_TOP,
       image_source_url: "https://via.placeholder.com/700x400&text=Carousel+image+#{index}",
       image_link_url: "https://leadership-is-mental-myklclason.c9users.io/?i=#{index}",
-      position: index * 10 + 10
+      position: index
     })
   end
+  CarouselImage.update_positions
 end
