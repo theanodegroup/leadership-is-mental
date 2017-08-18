5a070382e455d432bfca5ccc003d445d2431f07b
Mykl Clason <myklclason@gmail.com>
2017-08-18 04:21:03 +0000

    Fix bugs

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,7 @@ class Message < ActiveRecord::Base
   validates_length_of :name, :maximum => 100
   validates_length_of :email, :maximum => 120
   validates_length_of :content, :maximum => 500
+  validates_length_of :phone, :maximum => 15
 
   # Format Validations
   validates :content, format: {
@@ -11,5 +12,8 @@ class Message < ActiveRecord::Base
     message: "Only alphanumeric characters, whitespace and !#$%&*()-_:\";'/,.]*$ are allowed."
   }
 
-  validates :phone, phone: true, allow_blank: true
+  validates :phone, format: {
+    with: /\A[0-9()-+ ]*\z/,
+    message: "Only numeric characters, space, parantheses, plus sign and hyphen are allowed."
+  }
 end
