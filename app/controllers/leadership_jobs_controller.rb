0059d61c451b9015dac431821423647c2aefbce6
Mykl Clason <myklclason@gmail.com>
2017-08-24 01:25:40 +0000

    Update leadership job params controller whitelist

--- app/controllers/leadership_jobs_controller.rb
+++ app/controllers/leadership_jobs_controller.rb
@@ -69,6 +69,6 @@ class LeadershipJobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def leadership_job_params
-      params.require(:leadership_job).permit(:title, :description, :link, :pubdate)
+      params.require(:leadership_job).permit(:title, :description, :link, :pub_date, :source, :guid)
     end
 end
