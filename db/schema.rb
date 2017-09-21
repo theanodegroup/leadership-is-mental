dcb39a4bc483d7a877fe1dbe3f458eaddeb21ae9
Mykl Clason <myklclason@gmail.com>
2017-09-21 00:22:16 +0000

    Generate bootstrap themed leadership articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828043644) do
+ActiveRecord::Schema.define(version: 20170921002005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,17 @@ ActiveRecord::Schema.define(version: 20170828043644) do
   create_table "documents", force: :cascade do |t|
     t.string   "name"
     t.string   "doc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "leadership_articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "pub_date"
+    t.text     "summary"
+    t.string   "source"
+    t.string   "guid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
