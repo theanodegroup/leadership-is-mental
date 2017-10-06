6deed8511d4d1dd6206c379c48147cee39f96371
Mykl Clason <myklclason@gmail.com>
2017-10-06 01:44:58 +0000

    Add default sort order to contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,6 @@ class Contact < ActiveRecord::Base
   validates_format_of :email, :with => Devise::email_regexp
   validates_length_of :name, :maximum => 100
   validates_length_of :email, :maximum => 120
+
+  default_scope { order(created_at: :desc) }
 end
