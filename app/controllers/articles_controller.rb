a608c9b0c2d8facfcaa6a05d82e92a62b7fdda60
Mykl Clason <myklclason@gmail.com>
2017-11-24 23:17:19 +0000

    Allow users to view the article show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new, :show, :edit, :update, :destroy] # Require admin
+  before_action :authenticate_user!, except: [:show]
 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
